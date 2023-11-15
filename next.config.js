6793b04a411e5cd4439e940cf1746ed37d6cb5c6
NVH <93785793+ngvihoa@users.noreply.github.com>
2023-11-15 11:12:17 +0700

    chore: Init codebase

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+import { join } from "path";
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [join(__dirname, "styles")],
+  },
+};
+
+export default nextConfig;
