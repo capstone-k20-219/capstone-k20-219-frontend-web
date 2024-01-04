1e25b1543e342e74f842a6ae40821e69e3e067f3
NVH <93785793+ngvihoa@users.noreply.github.com>
2024-01-04 16:53:08 +0700

    feat: add realtime clock to header

--- src/app/(manager)/_ui/Header.tsx
+++ src/app/(manager)/_ui/Header.tsx
@@ -9,15 +9,26 @@ import Image from "next/image";
 import { stringify } from "querystring";
 
 export default function Header() {
-  const [date, SetDate] = useState(new Date());
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    setInterval(() => setDate(new Date()), 1000);
+  }, []);
 
   return (
     <div className="w-full h-20 px-6 py-4 bg-white justify-center items-center flex">
       <div className="grow shrink basis-0 self-stretch justify-between items-center inline-flex">
         <div className="justify-start items-center flex">
           <div className="text-neutral-500 text-sm font-medium">
-            {/* Thu 12/10/2023 12:24:32 */}
-            {`${date}`}
+            {date.toLocaleDateString("en-GB", {
+              weekday: "short",
+              day: "numeric",
+              month: "numeric",
+              year: "numeric",
+              hour: "numeric",
+              minute: "numeric",
+              second: "numeric",
+            })}
           </div>
         </div>
         <div className="flex-col justify-start items-center inline-flex">
--- src/app/(manager)/layout.tsx
+++ src/app/(manager)/layout.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect, useState } from "react";
 // Assets
 import Logo from "../../img/logo-white.png";
 import Header from "./_ui/Header";
@@ -10,15 +13,23 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="w-full h-dvh relative bg-stone-50 flex">
-      {/* sidebar */}
-      <Sidebar />
-      {/* header */}
-      <div className="flex flex-col w-full h-full">
-        <Header />
-        <div className="overflow-hidden">{children}</div>
+  const [inital, setInitial] = useState(false);
+
+  useEffect(() => {
+    setInitial(true);
+  }, []);
+
+  if (!inital) return null;
+  else
+    return (
+      <div className="w-full h-dvh relative bg-stone-50 flex">
+        {/* sidebar */}
+        <Sidebar />
+        {/* header */}
+        <div className="flex flex-col w-full h-full">
+          <Header />
+          <div className="overflow-hidden">{children}</div>
+        </div>
       </div>
-    </div>
-  );
+    );
 }
