1bc983905d9c5ef859237e293f21f31f9d18db1f
NVH <93785793+ngvihoa@users.noreply.github.com>
2024-03-06 23:32:10 +0700

    fix: Organize const, data, type defined, ...

---
+++ src/lib/data.tsx
@@ -0,0 +1,286 @@
+import {
+  EmployeeData,
+  NavItem,
+  ServiceData,
+  ServiceRequestData,
+  VehicleTypeData,
+} from "./type";
+import DashboardIcon from "@/img/dashboard.png";
+import MapIcon from "@/img/map.png";
+import EmployeeIcon from "@/img/employee.png";
+import ServiceIcon from "@/img/service.png";
+import VehicleTypeIcon from "@/img/vehicle-type.png";
+import HelpIcon from "@/img/help.png";
+import SettingIcon from "@/img/setting.png";
+import { TbChartTreemap, TbLayoutDashboard } from "react-icons/tb";
+import { MdOutlineDesignServices } from "react-icons/md";
+import { BsPeople } from "react-icons/bs";
+import { IoCarSport, IoSettingsOutline } from "react-icons/io5";
+import { IoMdHelpCircleOutline } from "react-icons/io";
+
+export const temporaryLoginInfo = [
+  {
+    id: "jbfaue89869",
+    username: "Manager",
+    password: "manager123",
+    role: "m",
+  },
+  {
+    id: "lkbhlkblbliua86",
+    username: "Employee",
+    password: "employee123",
+    role: "e",
+  },
+];
+
+export const ServiceRequestRows: ServiceRequestData[] = [
+  {
+    plate: "HP234-23.444",
+    phone: "0983786674",
+    time: "12:30 29/10/2023",
+  },
+  {
+    plate: "HP234-23.444",
+    phone: "0983786674",
+    time: "12:30 29/10/2023",
+  },
+  {
+    plate: "HP234-23.444",
+    phone: "0983786674",
+    time: "12:30 29/10/2023",
+  },
+  {
+    plate: "HP234-23.444",
+    phone: "0983786674",
+    time: "12:30 29/10/2023",
+  },
+];
+export const ServiceRequestRows2: ServiceRequestData[] = [
+  {
+    plate: "HP234-23.444",
+    phone: "0983786674",
+    time: "12:30 29/10/2023",
+  },
+  {
+    plate: "HP234-23.444",
+    phone: "0983786674",
+    time: "12:30 29/10/2023",
+  },
+];
+export const ServiceRequestRows3: ServiceRequestData[] = [
+  {
+    plate: "HP234-23.444",
+    phone: "0983786674",
+    time: "12:30 29/10/2023",
+  },
+];
+
+export const mockData: {
+  [key: string]: any;
+  washing: ServiceRequestData[] | null;
+  charging: ServiceRequestData[] | null;
+  maintaining: ServiceRequestData[] | null;
+} = {
+  washing: ServiceRequestRows,
+  charging: ServiceRequestRows2,
+  maintaining: null,
+};
+
+export const ServiceName = ["Washing", "Charging", "Maintaining"];
+
+export const employeeRows: EmployeeData[] = [
+  {
+    id: "MPL23456",
+    name: "Nguyen Huu Duc",
+    phone: "0983786674",
+  },
+  {
+    id: "MPL23456",
+    name: "Nguyen Huu Duc",
+    phone: "0983786674",
+  },
+  {
+    id: "MPL23456",
+    name: "Nguyen Huu Duc",
+    phone: "0983786674",
+  },
+  {
+    id: "MPL23456",
+    name: "Nguyen Huu Duc",
+    phone: "0983786674",
+  },
+  {
+    id: "MPL23456",
+    name: "Nguyen Huu Duc",
+    phone: "0983786674",
+  },
+  {
+    id: "MPL23456",
+    name: "Nguyen Huu Duc",
+    phone: "0983786674",
+  },
+  {
+    id: "MPL23456",
+    name: "Nguyen Huu Duc",
+    phone: "0983786674",
+  },
+  {
+    id: "MPL23456",
+    name: "Nguyen Huu Duc",
+    phone: "0983786674",
+  },
+];
+
+export const ServiceRows: ServiceData[] = [
+  {
+    name: "Charging",
+    price: 10,
+    typ: "E-car",
+  },
+  {
+    name: "Washing",
+    price: 10,
+    typ: "Car",
+  },
+  {
+    name: "Washing",
+    price: 6,
+    typ: "Motobike",
+  },
+  {
+    name: "Maintenance",
+    price: 25,
+    typ: "Car",
+  },
+];
+
+export const vehicleTypeRows: VehicleTypeData[] = [
+  {
+    id: "12345",
+    name: "Motobike",
+    bookingFee: 2,
+    parkingFee: 4,
+  },
+  {
+    id: "12346",
+    name: "Bikecycle",
+    bookingFee: 2,
+    parkingFee: 4,
+  },
+  {
+    id: "12347",
+    name: "Car",
+    bookingFee: 2,
+    parkingFee: 4,
+  },
+  {
+    id: "12348",
+    name: "Truck",
+    bookingFee: 2,
+    parkingFee: 4,
+  },
+];
+
+export const mockDataProfile = {
+  id: "MPL2354",
+  role: "Manager",
+  username: "Nguyen Huu Duc",
+  fullname: "Nguyen Huu Duc",
+  dob: "2002-07-29",
+  password: "Hoa29072002",
+} as const;
+
+const styleIcon = {
+  width: 24,
+  height: 24,
+  color: "#fff",
+};
+
+export const NavListItemMainManager: NavItem[] = [
+  {
+    name: "Dashboard",
+    icon: <TbLayoutDashboard style={styleIcon} />,
+    link: "/m-home",
+  },
+  {
+    name: "Map",
+    icon: <TbChartTreemap style={styleIcon} />,
+    link: "/m-map",
+  },
+  {
+    name: "Employee",
+    icon: <BsPeople style={styleIcon} />,
+    link: "/m-employee",
+  },
+  {
+    name: "Vehicle",
+    icon: <IoCarSport style={styleIcon} />,
+    link: "/m-vehicle",
+  },
+  {
+    name: "Service",
+    icon: <MdOutlineDesignServices style={styleIcon} />,
+    link: "/m-service",
+  },
+];
+
+export const NavListItemMainEmployee: NavItem[] = [
+  {
+    name: "Map",
+    icon: <TbChartTreemap style={styleIcon} />,
+    link: "/e-map",
+  },
+  {
+    name: "Service",
+    icon: <MdOutlineDesignServices style={styleIcon} />,
+    link: "/e-service",
+  },
+];
+
+export const NavListItemSecondary: NavItem[] = [
+  {
+    name: "Help",
+    icon: <IoMdHelpCircleOutline style={styleIcon} />,
+    link: "",
+  },
+  {
+    name: "Setting",
+    icon: <IoSettingsOutline style={styleIcon} />,
+    link: "",
+  },
+];
+
+export const ListNavEmployee = ["Map", "Service", "Help", "Setting"];
+
+export const ListNavManager = [
+  "Dashboard",
+  "Map",
+  "Employee",
+  "Vehicle Type",
+  "Service",
+  "Help",
+  "Setting",
+];
+
+export const SmallStatistics = [
+  {
+    name: "Current vehicle",
+    value: "60",
+  },
+  {
+    name: "Current available slot",
+    value: "34",
+  },
+  {
+    name: "Today booking",
+    value: "12",
+  },
+  {
+    name: "Yesterday revenue",
+    value: "$974,99",
+  },
+  {
+    name: "Total service",
+    value: "5",
+  },
+];
---
+++ src/lib/type.ts
@@ -0,0 +1,125 @@
+import { IconType } from "react-icons";
+
+export enum TableType {
+  Employee,
+  VehicleType,
+  Service,
+  ServiceRequest,
+}
+
+// Type for Employee Table
+export interface EmployeeColumn {
+  id: "id" | "name" | "phone" | "action";
+  label: string;
+  minWidth?: number;
+  align?: "center" | "left" | "right" | "justify" | "char" | undefined;
+  format?: (value: number) => string;
+}
+
+export interface EmployeeData {
+  [key: string]: any;
+  id: string;
+  name: string;
+  phone: string;
+}
+
+// Type for Vehicle Type Table
+export interface VehicleTypeColumn {
+  id: "id" | "name" | "bookingFee" | "parkingFee" | "action";
+  label: string;
+  minWidth?: number;
+  align?: "center" | "left" | "right" | "justify" | "char" | undefined;
+  format?: (value: number) => string;
+}
+
+export interface VehicleTypeData {
+  [key: string]: any;
+  name: string;
+  bookingFee: number;
+  parkingFee: number;
+}
+
+// Type for Service Table
+export interface ServiceColumn {
+  id: "name" | "price" | "typ" | "action";
+  label: string;
+  minWidth?: number;
+  align?: "center" | "left" | "right" | "justify" | "char" | undefined;
+  format?: (value: number) => string;
+}
+
+export interface ServiceData {
+  [key: string]: any;
+  name: string;
+  price: number;
+  typ: string;
+}
+
+// Type for Service Request Table
+export interface ServiceRequestColumn {
+  id: "plate" | "phone" | "time" | "action";
+  label: string;
+  minWidth?: number;
+  align?: "center" | "left" | "right" | "justify" | "char" | undefined;
+  format?: (value: number) => string;
+}
+
+export interface ServiceRequestData {
+  [key: string]: any;
+  plate: string;
+  phone: string;
+  time: string;
+}
+
+// Type for Table Data
+export type tableColumn =
+  | EmployeeColumn
+  | ServiceColumn
+  | VehicleTypeColumn
+  | ServiceRequestColumn;
+export type TableData =
+  | EmployeeData[]
+  | VehicleTypeData[]
+  | ServiceData[]
+  | ServiceRequestData[]
+  | null;
+
+// Type for input component
+export interface InputComponentType {
+  label: string;
+  value?: string;
+  type: string;
+  name: string;
+  placeholder?: string;
+  disable?: boolean;
+  autoFocus?: boolean;
+  required?: boolean;
+  onChangeFunction?: (value: any) => void;
+}
+
+// Type for Sidebar nav items
+export interface NavItem {
+  name: string;
+  icon?: JSX.Element;
+  link: string;
+}
+
+// Type for Auth Reducer _ auth-slice
+export type InitialState = {
+  value: AuthState;
+};
+
+export type RoleType = "m" | "e" | null;
+
+export type AuthState = {
+  isAuth: boolean;
+  username: string;
+  uid: string;
+  role: RoleType;
+};
+
+export type LoginType = {
+  username: string;
+  id: string;
+  role: RoleType;
+};
